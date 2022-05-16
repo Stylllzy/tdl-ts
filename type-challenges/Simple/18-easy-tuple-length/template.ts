07cd6723d9deb8c0318fcf5150d54157466e144d
Stylllzy <1184271384@qq.com>
2022-05-16 09:45:44 +0800

    0516 easy-tuple-length

---
+++ type-challenges/Simple/18-easy-tuple-length/template.ts
@@ -0,0 +1,10 @@
+type Length<T extends readonly any[]> = T["length"]
+
+
+
+// js
+// function arrLength(arr) {
+//   // 判断是否是数组，不是则return
+//   if(!Array.isArray(arr)) return;
+//   return arr.length
+// }
